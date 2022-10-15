854e84ae9ccea034f4b62ac4526f33a382bb5909
Egor Kaeta <kaeta.egor2015@yandex.ru>
2022-10-16 02:23:35 +0300

    feat: complete task dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  let DreamTeamName = []
+  if (Array.isArray(members)) {
+    for (let i=0; i<members.length; i++) {
+      if (typeof members[i] == 'string') {
+        let name = members[i].replace(/\s+/g, '')
+        DreamTeamName.push(name[0].toUpperCase())
+      }
+    }
+    DreamTeamName.sort()
+    DreamTeamName =  DreamTeamName.join('')
+    return DreamTeamName
+  }
+  else {
+    return false
+  }
 }
 
 module.exports = {
