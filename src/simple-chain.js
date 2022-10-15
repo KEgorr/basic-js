68d6cb773e4de75b310c09317692ec0fc0373ca3
Egor Kaeta <kaeta.egor2015@yandex.ru>
2022-10-16 02:23:57 +0300

    feat: complete task simple chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  newArr: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.newArr.length
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    this.newArr.push(`( ${value} )`)
+    return this
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (this.newArr[position-1] != undefined) {
+      this.newArr.splice(position-1, 1)
+      return this
+    }
+    else {
+      this.newArr = []
+      throw new Error ("You can\'t remove incorrect link!")
+    }
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.newArr.reverse()
+    return this
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let finishedChain = this.newArr.slice()
+    this.newArr = []
+    return finishedChain.join("~~")
   }
 };
 
