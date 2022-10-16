4e9e3a76174693c00a93d337d89f44f0704ca16e
Egor Kaeta <kaeta.egor2015@yandex.ru>
2022-10-16 21:18:01 +0300

    feat: complete task extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,42 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let tempoArrMain = []
+  let tempoArrForAdd = []
+  if (String(options.addition) != "undefined") {
+    tempoArrForAdd.push(String(options.addition))
+    if ("additionRepeatTimes" in options) {
+      for (let j = 1; j<options.additionRepeatTimes; j++) {
+        tempoArrForAdd.push(String(options.addition))
+      }
+  }
+  }
+  if ("additionSeparator" in options) {
+    tempoArrForAdd = tempoArrForAdd.join(options.additionSeparator)
+  }
+  else {
+    tempoArrForAdd = tempoArrForAdd.join("|")
+  }
+  if (String(str) != "undefined") {
+    tempoArrMain.push([String(str)])
+    tempoArrMain[0].push(tempoArrForAdd)
+    tempoArrMain[0] = tempoArrMain[0].join("")
+    if ("repeatTimes" in options) {
+      for (let i = 1; i<options.repeatTimes; i++) {
+        tempoArrMain.push([String(str)])
+        tempoArrMain[i].push(tempoArrForAdd)
+        tempoArrMain[i] = tempoArrMain[i].join("")
+      }
+    }
+  }
+  if ("separator" in options) {
+    tempoArrMain = tempoArrMain.join(options.separator)
+  }
+  else {
+    tempoArrMain = tempoArrMain.join("+")
+  }
+  return tempoArrMain
 }
 
 module.exports = {
